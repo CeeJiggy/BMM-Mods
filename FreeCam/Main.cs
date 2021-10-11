f7b5fc16270948c79513f39b27c4e9717590afab
Chase Brandes <chasebrandes@yahoo.com>
2021-10-11 14:04:07 -0400

    Add project files.

---
+++ FreeCam/Main.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using Flash2;
+using UnhollowerRuntimeLib;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace SkipReplay
+{
+    public static class Main
+    {
+        private static CameraController _cameraController;
+        private static SkipReplay _skipReplay;
+        
+        public static List<Type> OnModLoad(Dictionary<string, object> settings)
+        {
+            return new List<Type> { typeof(SkipReplay) };
+        }
+
+        public static void OnModUpdate()
+        {
+            var player = Object.FindObjectOfType<Player>();
+            if (player == null)
+            {
+                Object.Destroy(_skipReplay);
+                _skipReplay = null;
+                return;
+            }
+
+            if (player != null)
+            {
+                if (_skipReplay == null)
+                {
+                    _cameraController = player.GetCameraController();
+                    _skipReplay = new SkipReplay(_cameraController.gameObject.AddComponent(Il2CppType.Of<SkipReplay>()).Pointer);
+                }
+            }
+        }
+    }
+}
---
+++ FreeCam/SkipReplay.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Flash2;
+using Framework.UI;
+using UnityEngine;
+
+namespace SkipReplay
+{
+    internal class SkipReplay : MonoBehaviour
+    {
+        public SkipReplay(IntPtr value) : base(value) { }
+
+        private void Start()
+        {
+            MainGame.s_Instance.m_ReplayParam.FalloutReplayTime = 0f;
+            MainGame.s_Instance.m_ReplayParam.FalloutReplayRounds = 0;
+        }
+    }
+}
